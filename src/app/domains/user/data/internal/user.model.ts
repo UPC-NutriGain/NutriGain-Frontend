3fea5db572726a9862a8813d8cb53347e45d9e18
nicxlxs <122683587+nicxlxs@users.noreply.github.com>
2025-07-08 18:01:38 -0500

    Delete src directory

--- src/app/domains/user/data/internal/user.model.ts
+++
@@ -1,9 +0,0 @@
-// src/app/domains/user/data/internal/user.model.ts
-export interface User {
-  id: number;
-  nombre: string;
-  email: string;
-  password: string;
-  edad: number;
-  caminoSeleccionado: number;
-}
